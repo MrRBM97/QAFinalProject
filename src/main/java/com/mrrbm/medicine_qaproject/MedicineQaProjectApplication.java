a20d7f7ea54738b472b358ffacc04e7684408a29
MrRBM97 <rossmartin97@outlook.com>
2021-10-06 20:51:57 +0100

    Initialised Maven/Spring

---
+++ src/main/java/com/mrrbm/medicine_qaproject/MedicineQaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mrrbm.medicine_qaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineQaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineQaProjectApplication.class, args);
+	}
+
+}
